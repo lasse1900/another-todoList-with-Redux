e975e69e882df3a36ff6f0ec2989c1a7fa442af0
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-11-19 14:55:20 +0200

    step 2 todo-list with Redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React with Redux</h2>
+        </div>
+        <div className="Todo-App">
+          <form>
+            <input type="text" />
+          </form>
+          <div className="Todo-List">
+            <ul>
+              <li>
+                <input type="checkbox" /> Create Static UI
+              </li>
+              <li>
+                <input type="checkbox" /> Create Initial State
+              </li>
+              <li>
+                <input type="checkbox" /> Use State to Render UI
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
