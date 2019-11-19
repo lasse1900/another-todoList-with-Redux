3ab57336ea8fec8da3493038ade7c4c613189b87
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-11-19 16:23:32 +0200

    step 6 create redux store

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './store'
 
 
-const state = {
-  todos: [
-    {id: 1, name: 'Render static UI', isComplete: true},
-    {id: 2, name: 'Create Initial State', isComplete: true},
-    {id: 3, name: 'Render based on state', isComplete: true},
-  ]
-}
+const state = store.getState()
 
-ReactDOM.render(<App todos={state.todos}/>, document.getElementById('root'));
+ReactDOM.render(<App {...state} />, document.getElementById('root'));
