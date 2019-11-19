87229cf708d9baf54018e51186e4d2a06f849555
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-11-19 16:51:34 +0200

    step 8 keep UI state in sync using controlled components with Redux

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import store from './store'
 
+const todoChangeHandler = (val) => store.dispatch({type: 'CURRENT_UPDATE', payload: val})
+
 const render = () => {
   const state = store.getState()
-  ReactDOM.render(<App {...state} />, document.getElementById('root'));
+  ReactDOM.render(<App todos={state.todos}
+    currentTodo={state.currentTodo}
+    changeCurrent={todoChangeHandler}
+     />, document.getElementById('root'));
 }
 render()
 
 store.subscribe(render)
 
-setTimeout(() => {
-  store.dispatch({ type: 'TODO_ADD', payload: { id: 4, name: 'New todo', isComplete: true} })
-}, 1000)
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,15 +1,18 @@
 const initState = {
   todos: [
-    {id:1, name: 'create a store', isComplete: true},
-    {id:2, name: 'Load state through the store', isComplete: true},
-    {id:3, name: 'Handle state changes with redux', isComplete: false}
-  ]
+    { id: 1, name: 'create a store', isComplete: true },
+    { id: 2, name: 'Load state through the store', isComplete: true },
+    { id: 3, name: 'Handle state changes with redux', isComplete: false }
+  ],
+  currentTodo: ''
 }
 
 export default (state = initState, action) => {
   switch (action.type) {
     case 'TODO_ADD':
-      return {...state, todos: state.todos.concat(action.payload)}
+      return { ...state, todos: state.todos.concat(action.payload) }
+    case 'CURRENT_UPDATE':
+        return { ...state, currentTodo: action.payload }
     default:
       return state
   }
