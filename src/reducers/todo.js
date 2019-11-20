6c6b4eb8fd1111a5aba3cb82b445963fa4594409
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-11-20 16:01:57 +0200

    step 17 save a new record on the server with Redux thunk and fetch

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,4 +1,4 @@
-import { getTodos } from '../lib/todoServices.js'
+import { getTodos, createTodo } from '../lib/todoServices.js'
 
 const initState = {
   todos: [],
@@ -11,6 +11,7 @@ const CURRENT_UPDATE = 'CURRENT_UPDATE'
 
 export const updateCurrent = (val) => ({ type: CURRENT_UPDATE, payload: val })
 export const loadTodos = (todos) => ({ type: TODOS_LOAD, payload: todos })
+export const addTodo = (todo) => ({ type: TODO_ADD, payload: todo })
 export const fetchTodos = () => {
   return (dispatch) => {
     getTodos()
@@ -18,12 +19,19 @@ export const fetchTodos = () => {
   }
 }
 
+export const saveTodo = (name) => {
+  return (dispatch) => {
+    createTodo(name)
+      .then(res => dispatch(addTodo(res)))
+  }
+}
+
 export default (state = initState, action) => {
   switch (action.type) {
     case TODO_ADD:
-      return { ...state, todos: state.todos.concat(action.payload) }
+      return { ...state, currentTodo: '', todos: state.todos.concat(action.payload) }
     case TODOS_LOAD:
-      return { ...state, todos: action.payload}
+      return { ...state, todos: action.payload }
     case CURRENT_UPDATE:
       return { ...state, currentTodo: action.payload }
     default:
