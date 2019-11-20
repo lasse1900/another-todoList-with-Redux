778f240b7eed77a8048842b935695ad8e1ebf55c
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-11-20 09:29:30 +0200

    step 10 dispatch functions from action creators with bindActionCreators

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './store'
+import { bindActionCreators } from 'redux'
 import { updateCurrent } from './reducers/todo'
 
-const todoChangeHandler = (val) =>
-  store.dispatch(updateCurrent(val))
+const actions = bindActionCreators({updateCurrent}, store.dispatch)
 
 const render = () => {
   const state = store.getState()
   ReactDOM.render(<App todos={state.todos}
     currentTodo={state.currentTodo}
-    changeCurrent={todoChangeHandler}
+    changeCurrent={actions.updateCurrent}
   />, document.getElementById('root'));
 }
 render()
