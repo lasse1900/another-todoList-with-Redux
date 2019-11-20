8ee74d443edf7c9bd4857f72f9c46eeaa34c2ef6
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-11-20 15:30:45 +0200

    step 16a dispatch async actions with Redux Thunk

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,23 +1,27 @@
+import { getTodos } from '../lib/todoServices.js'
+
 const initState = {
-  todos: [
-    { id: 1, name: 'create a store', isComplete: true },
-    { id: 2, name: 'Load state through the store', isComplete: true },
-    { id: 3, name: 'Handle state changes with redux', isComplete: false }
-  ],
+  todos: [],
   currentTodo: ''
 }
 
 const TODO_ADD = 'TOTO_ADD'
 const CURRENT_UPDATE = 'CURRENT_UPDATE'
 
-export const updateCurrent = (val) => ({type: CURRENT_UPDATE, payload: val})
+export const updateCurrent = (val) => ({ type: CURRENT_UPDATE, payload: val })
+export const fetchTodos = () => {
+  return () => {
+    getTodos()
+      .then(todos => console.log(todos))
+  }
+}
 
 export default (state = initState, action) => {
   switch (action.type) {
     case TODO_ADD:
       return { ...state, todos: state.todos.concat(action.payload) }
     case CURRENT_UPDATE:
-        return { ...state, currentTodo: action.payload }
+      return { ...state, currentTodo: action.payload }
     default:
       return state
   }
